ce8c5140dc2e6eca2a0c148320361640b1e70250
Holly Grimm <holly.grimm@gmail.com>
2021-01-19 14:12:07 -0700

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'stoodis'`, () => {
+  it(`should have as title 'Béeso'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('stoodis');
+    expect(app.title).toEqual('Béeso');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('stoodis app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Béeso app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,62 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChartsModule } from 'ng2-charts';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { DashComponent } from './dash/dash.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { CardComponent } from './card/card.component';
+import { IncomeSourceCoinChartComponent } from './charts/income-source-coin-chart/income-source-coin-chart.component';
+import { CoinAllocationChartComponent } from './charts/coin-allocation-chart/coin-allocation-chart.component';
+import { AnnualIncomeChartComponent } from './charts/annual-income-chart/annual-income-chart.component';
+import { IncomeSourceChartComponent } from './charts/income-source-chart/income-source-chart.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MiniCardComponent } from './mini-card/mini-card.component';
+import { RemindersComponent } from './tables/reminders/reminders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    DashComponent,
+    CardComponent,
+    IncomeSourceCoinChartComponent,
+    CoinAllocationChartComponent,
+    AnnualIncomeChartComponent,
+    IncomeSourceChartComponent,
+    TransactionsComponent,
+    MiniCardComponent,
+    RemindersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ChartsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
